f25fe6caf2720f3471ef380b88d430da79f6a965
Roman-Bobrovskiy <83470055+Roman-Bobrovskiy@users.noreply.github.com>
2021-10-05 17:20:17 +0300

    Add files via upload

--- js/Cuisines.js
+++ js/Cuisines.js
@@ -1,70 +1,78 @@
 const arrImages = [
   {
+    num: "1437",
     src: "./img/cuisines/american.png",
     alt: "american.png",
     theNameOfCuisines: "AMERICAN",
-    num: "1437",
   },
   {
+    num: "145",
     src: "./img/cuisines/chinese.png",
     alt: "chinese.png",
     theNameOfCuisines: "CHINESE",
-    num: "145",
   },
   {
+    num: "27",
     src: "./img/cuisines/french.png",
     alt: "french.png",
     theNameOfCuisines: "FRENCH",
-    num: "27",
   },
   {
+    num: "856",
     src: "./img/cuisines/indian.png",
     alt: "indian.png",
     theNameOfCuisines: "INDIAN",
-    num: "856",
   },
   {
+    num: "327",
     src: "./img/cuisines/italian.png",
     alt: "italian.png",
     theNameOfCuisines: "ITALIAN",
-    num: "327",
   },
   {
+    num: "529",
     src: "./img/cuisines/mexican.png",
     alt: "mexican.png",
     theNameOfCuisines: "MEXICAN",
-    num: "529",
   },
   {
+    num: "327",
     src: "./img/cuisines/pazza.png",
     alt: "pazza.png",
     theNameOfCuisines: "PIZZA",
-    num: "327",
   },
   {
+    num: "731",
     src: "./img/cuisines/seafood.png",
     alt: "seafood.png",
     theNameOfCuisines: "SEAFOOD",
-    num: "731",
   },
   {
+    num: "174",
     src: "./img/cuisines/steakhouse.png",
     alt: "steakhouse.png",
     theNameOfCuisines: "STEAKHOUSE",
-    num: "174",
   },
   {
+    num: "237",
     src: "./img/cuisines/sushi.png",
     alt: "sushi.png",
     theNameOfCuisines: "SUSHI",
-    num: "237",
   },
 ];
 
 let gallery = document.querySelector(".cuisinesGallery");
 let scrollPosition = 0;
 let ticking = false;
-let arrNum = [];
+
+//sort
+let sortArr = () => {
+  if (arrImages.length > 5) {
+    arrImages.sort((a, b) => {
+      return a.num - b.num;
+    });
+  }
+};
 
 //Markup
 let createMarkupGallery = (obj) => {
@@ -80,6 +88,7 @@ let createMarkupGallery = (obj) => {
 };
 
 const addImages = () => {
+  sortArr();
   return arrImages.map((obj) => {
     return createMarkupGallery(obj);
   });
